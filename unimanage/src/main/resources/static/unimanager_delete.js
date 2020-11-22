3d26ab55cbd36445a940afeca408f8d38ebb207f
Joaopandre <73540806+Joaopandre@users.noreply.github.com>
2020-11-22 22:44:14 +0000

    Create unimanager_delete.js

---
+++ unimanage/src/main/resources/static/unimanager_delete.js
@@ -0,0 +1,13 @@
+async function unimanager_del() {
+
+    try {
+         let number = document.getElementById("number").value;
+         let res = await $.ajax({ 
+             url: "/api/students/"+number, 
+             method: "delete", 
+             dataType: "text" 
+           }); 
+           document.getElementById("message").innerHTML = res; 
+       } catch (error) { console.log(error); } 
+
+}
